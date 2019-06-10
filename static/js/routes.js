b442e95156a8ad1720a370726ec0cc4d42d8a744
Lisia Dutt <ld034@hdm-stuttgart.de>
2019-06-10 21:26:32 +0200

    fixed functionality of navigation and styled closeBtn

--- static/js/routes.js
+++ static/js/routes.js
@@ -1,6 +1,7 @@
 
 function calcRoute(startLat, startLng, endLat, endLng) {
 
+    var button;
     // Create start and end location objects
     var start = {
         lat: startLat,
@@ -28,17 +29,19 @@ function calcRoute(startLat, startLng, endLat, endLng) {
         }
     });
 
-     // Create Button to close navigation
-     var button = document.createElement('button');
-     button.id = 'closeBtn'
-     button.innerHTML = 'Close Navigation';
-     map.controls[google.maps.ControlPosition.RIGHT_TOP].push(button);
+     // Create button to close navigation
+     if(!document.body.contains(document.getElementById('closeBtn'))){
+       button = document.createElement('div');
+       button.id = 'closeBtn'
+       button.innerHTML = 'Close Navigation';
+       map.controls[google.maps.ControlPosition.RIGHT_TOP].push(button);
 
-     // end navigation and go to current location by button click
-     button.addEventListener ("click", function() {
-       directionsDisplay.setMap(null);
-       navigator.geolocation.getCurrentPosition(success, error, options);
-       var button = document.getElementById('closeBtn');
-       button.parentNode.removeChild(button);
-     });
+       // stop navigation and go to current location by button click
+       button.addEventListener ("click", function() {
+         directionsDisplay.set('directions', null);
+         navigator.geolocation.getCurrentPosition(success, error, options);
+         var button = document.getElementById('closeBtn');
+         button.parentNode.removeChild(button);
+       });
+     }
 }
